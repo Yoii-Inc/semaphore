d5942fce8045cbc93aae9325cba6cf855d65d811
sheagrief <3a.mad1earth4@gmail.com>
2022-11-22 20:18:38 +0900

    revise index.ts

--- packages/proof/src/index.ts
+++ packages/proof/src/index.ts
@@ -3,6 +3,11 @@ import generateProof from "./generateProof"
 import verifyProof from "./verifyProof"
 import generateSignalHash from "./generateSignalHash"
 import packToSolidityProof from "./packToSolidityProof"
+import generateRoleRegisterNullifierHash from "./generateRoleRegisterNullifierHash"
+import generateRoleRegisterProof from "./generateRoleRegisterProof"
+import verifyRoleRegisterProof from "./verifyRoleRegisterProof"
+import generateRoleVerifyProof from "./generateRoleVerifyProof"
+import verifyRoleVerifyProof from "./verifyRoleVerifyProof"
 
-export { generateNullifierHash, generateProof, verifyProof, generateSignalHash, packToSolidityProof }
+export { generateNullifierHash, generateProof, verifyProof, generateSignalHash, packToSolidityProof, generateRoleRegisterNullifierHash, generateRoleRegisterProof, verifyRoleRegisterProof, generateRoleVerifyProof, verifyRoleVerifyProof }
 export * from "./types"
