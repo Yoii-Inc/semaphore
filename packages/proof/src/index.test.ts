af8fe3016efac17698ebb37bb3fcb90d8480777e
sheagrief <3a.mad1earth4@gmail.com>
2022-11-22 20:54:45 +0900

    revise proof test

--- packages/proof/src/index.test.ts
+++ packages/proof/src/index.test.ts
@@ -4,11 +4,16 @@ import { Identity } from "@semaphore-protocol/identity"
 import { getCurveFromName } from "ffjavascript"
 import fs from "fs"
 import generateNullifierHash from "./generateNullifierHash"
+import generateRoleRegisterNullifierHash from "./generateRoleRegisterNullifierHash"
 import generateProof from "./generateProof"
+import generateRoleRegisterProof from "./generateRoleRegisterProof"
+import generateRoleVerifyProof from "./generateRoleVerifyProof"
 import generateSignalHash from "./generateSignalHash"
 import packToSolidityProof from "./packToSolidityProof"
-import { FullProof } from "./types"
+import { FullProof, FullVerifyProof, FullRegisterProof } from "./types"
 import verifyProof from "./verifyProof"
+import verifyRoleRegisterProof from "./verifyRoleRegisterProof"
+import verifyRoleVerifyProof from "./verifyRoleVerifyProof"
 
 describe("Proof", () => {
     const treeDepth = Number(process.env.TREE_DEPTH) || 20
@@ -20,9 +25,17 @@ describe("Proof", () => {
     const zkeyFilePath = `./snark-artifacts/semaphore.zkey`
     const verificationKeyPath = `./snark-artifacts/semaphore.json`
 
+    const roleRegisterWasmFilePath = `./snark-artifacts/register.wasm`
+    const roleRegisterZkeyFilePath = `./snark-artifacts/register.zkey`
+
+    const roleVerifyWasmFilePath = `./snark-artifacts/verify.wasm`
+    const roleVerifyZkeyFilePath = `./snark-artifacts/verify.zkey`
+
     const identity = new Identity()
 
     let fullProof: FullProof
+    let fullRegisterProof: FullRegisterProof
+    let fullVerifyProof: FullVerifyProof
     let curve: any
 
     beforeAll(async () => {
@@ -128,4 +141,114 @@ describe("Proof", () => {
             expect(response).toBe(true)
         })
     })
+
+    describe("# generateRoleRegisterProof", () => {
+        it("Should generate a Register proof", async () => {
+            const role = BigInt(1)
+
+            const candidates = [BigInt(1),BigInt(2)]
+
+            fullRegisterProof = await generateRoleRegisterProof(identity, role, candidates, {
+                wasmFilePath: roleRegisterWasmFilePath,
+                zkeyFilePath: roleRegisterZkeyFilePath
+            })
+
+            expect(typeof fullRegisterProof).toBe("object")
+            expect(fullRegisterProof.publicRegisterSignals.candidates).toBe(candidates)
+        }, 20000)
+    })
+
+    describe("# generateRoleRegisterNullifierHash", () => {
+        it("Should generate a valid nullifier hash", async () => {
+            const nullifierHash = generateRoleRegisterNullifierHash(identity.getNullifier())
+
+            expect(nullifierHash.toString()).toBe(fullRegisterProof.publicRegisterSignals.nullifierHash)
+        })
+    })
+
+    describe("# verifyRoleRegisterProof", () => {
+        it("Should generate and verify a Reigster proof", async () => {
+            const verificationKey = JSON.parse(fs.readFileSync(verificationKeyPath, "utf-8"))
+
+            const response = await verifyRoleRegisterProof(verificationKey, fullRegisterProof)
+
+            expect(response).toBe(true)
+        })
+    })
+
+    describe("# generateRoleVerifyProof", () => {
+        it("Should not generate Semaphore proofs if the identity is not part of the group", async () => {
+            const group = new Group(treeDepth)
+
+            group.addMembers([BigInt(1), BigInt(2)])
+
+            const fun = () =>
+                generateRoleVerifyProof(identity, group, BigInt(3), [BigInt(2)],  externalNullifier, signal, {
+                    wasmFilePath: roleVerifyWasmFilePath,
+                    zkeyFilePath: roleVerifyZkeyFilePath
+                })
+
+            await expect(fun).rejects.toThrow("The identity is not part of the role")
+        })
+
+        it("Should not generate a Semaphore proof with default snark artifacts with Node.js", async () => {
+            const group = new Group(treeDepth)
+
+            group.addMembers([BigInt(1), BigInt(2), identity.commitment])
+
+            const role = BigInt(1)
+            const candidates = [BigInt(1), BigInt(2)]
+
+            const fun = () => generateRoleVerifyProof(identity, group, role, candidates, externalNullifier, signal)
+
+            await expect(fun).rejects.toThrow("ENOENT: no such file or directory")
+        })
+
+        it("Should generate a Semaphore proof passing a group as parameter", async () => {
+            const group = new Group(treeDepth)
+
+            group.addMembers([BigInt(1), BigInt(2), identity.commitment])
+
+            const role = BigInt(1)
+            const candidates = [BigInt(1), BigInt(2)]
+
+            fullVerifyProof = await generateRoleVerifyProof(identity, group, role, candidates, externalNullifier, signal, {
+                wasmFilePath: roleVerifyWasmFilePath,
+                zkeyFilePath: roleVerifyZkeyFilePath
+            })
+
+            expect(typeof fullVerifyProof).toBe("object")
+            expect(fullVerifyProof.publicVerifySignals.externalNullifier).toBe(externalNullifier)
+            expect(fullVerifyProof.publicVerifySignals.merkleRoot).toBe(group.root.toString())
+        }, 20000)
+
+        it("Should generate a Semaphore proof passing a Merkle proof as parametr", async () => {
+            const group = new Group(treeDepth)
+
+            group.addMembers([BigInt(1), BigInt(2), identity.commitment])
+
+            const role = BigInt(1)
+            const candidates = [BigInt(1), BigInt(2)]
+
+            fullVerifyProof = await generateRoleVerifyProof(identity, group.generateProofOfMembership(2), role, candidates, externalNullifier, signal, {
+                wasmFilePath: roleVerifyWasmFilePath,
+                zkeyFilePath: roleVerifyZkeyFilePath
+            })
+
+            expect(typeof fullVerifyProof).toBe("object")
+            expect(fullVerifyProof.publicVerifySignals.externalNullifier).toBe(externalNullifier)
+            expect(fullVerifyProof.publicVerifySignals.merkleRoot).toBe(group.root.toString())
+        }, 20000)
+    })
+
+    describe("# verifyRoleVerifyProof", () => {
+        it("Should generate and verify a RoleVerify proof", async () => {
+            const verificationKey = JSON.parse(fs.readFileSync(verificationKeyPath, "utf-8"))
+
+            const response = await verifyRoleVerifyProof(verificationKey, fullVerifyProof)
+
+            expect(response).toBe(true)
+        })
+    })
+
 })
